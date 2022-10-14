071d77ded924e8c34cc99fae23bfcdb0a7f0a969
ice.breaker <ice.breaker@trigram.co>
2022-10-14 10:35:32 -0500

    chore: Pull in new client, cleanup settings code.

--- nodejs/simple-cli/register.js
+++ nodejs/simple-cli/register.js
@@ -5,76 +5,61 @@
 /// keypair using [w3up-cli](https://github.com/web3-storage/w3up-cli), you can create the `settings.json`
 /// file using `w3up-cli export-settings` instead of using this script.
 
+import { createClient, exportSettings } from "@web3-store/w3up-client";
 
-import { createClient } from 'w3up-client'
-
-import fs from 'fs'
-import path from 'path'
+import fs from "fs";
+import path from "path";
 
 /// For simplicity, we always write to `settings.json` in the current directory
-const OUTPUT_FILENAME = 'settings.json'
+const OUTPUT_FILENAME = "settings.json";
 
 async function main() {
   /// To keep things simple, we just bail out if the output file exists rather than prompt to overwrite, etc.
   if (fs.existsSync(OUTPUT_FILENAME)) {
-    console.error(`Output file ${OUTPUT_FILENAME} exists. Please move it elsewhere and try again.`)
-    process.exit(1)
+    console.error(
+      `Output file ${OUTPUT_FILENAME} exists. Please move it elsewhere and try again.`
+    );
+    process.exit(1);
   }
 
   /// We take a single command line argument (the email to register)
   if (process.argv.length !== 3) {
-    const scriptFile = path.relative(process.cwd(), process.argv[1])
-    console.error(`usage: node ${scriptFile} <email-address>`)
-    process.exit(1)
+    const scriptFile = path.relative(process.cwd(), process.argv[1]);
+    console.error(`usage: node ${scriptFile} <email-address>`);
+    process.exit(1);
   }
-  const email = process.argv[2]
+  const email = process.argv[2];
 
   /// - Create a new w3up client
-  const client = createClient({
-    serviceDID: 'did:key:z6MkrZ1r5XBFZjBU34qyD8fueMbMRkKw17BZaq2ivKFjnz2z',
-    serviceURL: 'https://8609r1772a.execute-api.us-east-1.amazonaws.com',
-    accessDID: 'did:key:z6MkkHafoFWxxWVNpNXocFdU6PL2RVLyTEgS1qTnD3bRP7V9',
-    accessURL: 'https://access-api.web3.storage',
-    settings: new Map(),
-  })
+  const client = createClient();
 
   /// - Call client.register
   try {
-    console.log(`Registering email address ${email}. Please check your inbox. If you don't see the email, check your spam folder.`)
-    const id = await client.identity()
-    await client.register(email)
-    
-    console.log(`Success! Registered email ${email} with id ${id.did()}`)
+    console.log(
+      `Registering email address ${email}. Please check your inbox. If you don't see the email, check your spam folder.`
+    );
+    const id = await client.identity();
+    await client.register(email);
+
+    console.log(`Success! Registered email ${email} with id ${id.did()}`);
     /// - On success, call saveSettings to write settings.json
-    await saveSettings(client.settings)
+    await saveSettings(client.settings);
   } catch (err) {
-    console.error(`Registration error: ${err}`)
-    process.exit(1)
+    console.error(`Registration error: ${err}`);
+    process.exit(1);
   }
 }
-
 
 /**
  * Saves the identity settings to a JSON file.
- * 
- * @param {Map<string, unknown>} settings 
+ *
+ * @param {Map<string, unknown>} settings
  */
 async function saveSettings(settings) {
-  const email = settings.get('email')
-  const secret = settings.get('secret')
-  if (!email || !settings) {
-    throw new Error(`unable to save settings: no registered id`)
-  }
-
-  const buf = Buffer.from(secret)
-  const secretBase64 = buf.toString('base64')
-  const jsonText = JSON.stringify({
-    email,
-    secret: secretBase64
-  })
-  await fs.promises.writeFile(OUTPUT_FILENAME, jsonText, 'utf-8')
+  const exported = exportSettings(settings);
+  const jsonText = JSON.stringify(exported);
+  await fs.promises.writeFile(OUTPUT_FILENAME, jsonText, "utf-8");
 }
 
-
 /// call the entry point function:
-main()
+main();
--- nodejs/simple-cli/upload.js
+++ nodejs/simple-cli/upload.js
@@ -1,25 +1,25 @@
 /// This example project aims to demonstrate how to integrate the `w3up-client` SDK
-/// into a node command line app. 
+/// into a node command line app.
 ///
 /// To keep the example focused, we'll be leaving a lot of things out that you'd
 /// probably want to have in a "production-ready" application. For a real-world
 /// CLI example, see https://github.com/web3-storage/w3up-cli
 
 /// First we need to import the `createClient` function from the `w3up-client` package.
-import { createClient } from 'w3up-client'
+import { createClient } from "@web3-storage/w3up-client";
 
-/// We also import the `packToBob` function from the `ipfs-car` package, 
-/// to create IPFS-formatted file objects and pack them into a Content Archive (CAR) for uploading. 
+/// We also import the `packToBob` function from the `ipfs-car` package,
+/// to create IPFS-formatted file objects and pack them into a Content Archive (CAR) for uploading.
 /// Note that this will likely be handled automatically by a future version of `w3up-client`.
-import { packToBlob } from 'ipfs-car/pack/blob'
+import { packToBlob } from "ipfs-car/pack/blob";
 
 /// Finally, we import a few things from the node standard library
-import fs from 'fs'
-import path from 'path'
+import fs from "fs";
+import path from "path";
 
 /// In this example, we always load the identity key from a `settings.json` file
 /// in the current directory. See the `loadSettings` function below.
-const SETTINGS_FILENAME = 'settings.json'
+const SETTINGS_FILENAME = "settings.json";
 
 /**
  * The entry-point function is invoked at the bottom of this file.
@@ -27,56 +27,58 @@ const SETTINGS_FILENAME = 'settings.json'
 async function main() {
   // To keep things super simple, we only support a single command line argument: the path to a file.
   // We need to slice off the first two elements of process.args, which contain the nodejs command and path to the script file.
-  const args = process.argv.slice(2)
+  const args = process.argv.slice(2);
 
   // Bail out if we have an unexpected number of arguments
   if (args.length !== 1) {
-    const scriptPath = path.relative(process.cwd(), process.argv[1])
-    console.error(`usage: node ${scriptPath} <filename>`)
-    process.exit(1)
+    const scriptPath = path.relative(process.cwd(), process.argv[1]);
+    console.error(`usage: node ${scriptPath} <filename>`);
+    process.exit(1);
   }
 
-  const filename = args[0]
+  const filename = args[0];
 
   // Load the secret key from a local settings.json file.
   // If it does not exist, `loadIdentity` will prompt the user to
   // register an identity using the `register.js` script.
-  const { secret, email } = await loadIdentity()
+  const settings = await loadIdentity();
 
   // create a new w3up client using the secret key
-  const client = makeClientWithRegisteredSecretKey(secret)
+  const client = createClient({ settings });
 
   // client.identity() returns an object we can use to print the `did:key:`
   // string that is used as our public identity.
-  const id = await client.identity()
-  console.log(`Uploading as ${id.did()} (${email})`)
+  const { agent, account } = await client.identity();
+  console.log(`Uploading as ${account.did()} with (${agent.did()})`);
 
-  const { rootCID, carBytes } = await prepareForUpload(filename)
-  console.log(`Uploading ${filename}`)
-  console.log(`Root CID: ${rootCID}\n`)
+  const { rootCID, carBytes } = await prepareForUpload(filename);
+  console.log(`Uploading ${filename}`);
+  console.log(`Root CID: ${rootCID}\n`);
 
   try {
-    await client.upload(carBytes)
-    console.log('🎉 Upload complete!')
+    await client.upload(carBytes);
+    console.log("🎉 Upload complete!");
 
-    const gatewayURL = `https://w3s.link/ipfs/${rootCID}`
-    console.log(`🔗 View on an IPFS <-> HTTP gateway at ${gatewayURL}`)
-    console.log('Note that it may take a few seconds before content is available.')
+    const gatewayURL = `https://w3s.link/ipfs/${rootCID}`;
+    console.log(`🔗 View on an IPFS <-> HTTP gateway at ${gatewayURL}`);
+    console.log(
+      "Note that it may take a few seconds before content is available."
+    );
   } catch (err) {
-    console.error(`🧨 Upload failed with an error: ${err}`)
+    console.error(`🧨 Upload failed with an error: ${err}`);
   }
 }
 
 /**
  * Loads the identity key and registered email address from a `settings.json` file in the current directory.
  * If the file does not exist, prints an error message and exits the process.
- * 
- * @returns {Promise<{email: string, secret: Uint8Array}>} an object containing the secret key and registered email address
+ *
+ * @returns {Promise<string>} an object containing the secret key and registered email address
  */
 async function loadIdentity() {
   if (!fs.existsSync(SETTINGS_FILENAME)) {
-    const registerScriptPath = path.relative(process.cwd(), 'register.js')
-    
+    const registerScriptPath = path.relative(process.cwd(), "register.js");
+
     const msg = `
       To upload files, you first need to create an identity key and register it.
       This example script expects to load the key from a file named ${SETTINGS_FILENAME}
@@ -90,48 +92,25 @@ async function loadIdentity() {
       2) using an identity you've already registered with w3up-cli (https://github.com/web3-storage/w3up-cli)
 
       \t w3up-cli export-settings settings.json
-    `.replace(/^ +/gm, '') // strip leading spaces from each line, but keep tabs
+    `.replace(/^ +/gm, ""); // strip leading spaces from each line, but keep tabs
 
-    console.error(msg)
-    process.exit(1)
+    console.error(msg);
+    process.exit(1);
   }
 
   try {
-    const jsonText = await fs.promises.readFile(SETTINGS_FILENAME, 'utf-8')
-    const settings = JSON.parse(jsonText)
-    // decode the base64-encoded secret key
-    const secret = Uint8Array.from(Buffer.from(settings.secret, 'base64'))
-    const email = settings.email
-    return { email, secret }
+    return await fs.promises.readFile(SETTINGS_FILENAME, "utf-8");
   } catch (err) {
-    console.error('Error parsing secret key from settings file:', err)
-    process.exit(1)
+    console.error("Error parsing secret key from settings file:", err);
+    process.exit(1);
   }
-}
-
-/**
- * Creates a w3up Client object using the given private signing key.
- * @param {Uint8Array} secret a secret signing key that has been registered with the w3up access service.
- * @returns {import('w3up-client').Client} A w3up Client object configured to use the given key to authorize uploads.
- */
-function makeClientWithRegisteredSecretKey(secret) {
-  const settings = new Map()
-  settings.set('secret', secret)
-
-  return createClient({
-    serviceDID: 'did:key:z6MkrZ1r5XBFZjBU34qyD8fueMbMRkKw17BZaq2ivKFjnz2z',
-    serviceURL: 'https://8609r1772a.execute-api.us-east-1.amazonaws.com',
-    accessDID: 'did:key:z6MkkHafoFWxxWVNpNXocFdU6PL2RVLyTEgS1qTnD3bRP7V9',
-    accessURL: 'https://access-api.web3.storage',
-    settings,
-  })
 }
 
 /**
  * Takes the path to a file and creates a CAR with a an IPFS (UnixFS) formatted file object, wrapped in a directory so that we can preserve the filename.
- *  
+ *
  * @param {string} filename - the path to a local file to be uploaded
- * 
+ *
  * @returns {Promise<{ rootCID: string, carBytes: Uint8Array }>}
  */
 async function prepareForUpload(filename) {
@@ -140,30 +119,30 @@ async function prepareForUpload(filename) {
   // https://github.com/ipfs/js-ipfs/blob/89aeaf8e25320276391653104981e37a73f29de9/packages/ipfs-core-types/src/utils.ts#L30
 
   // The `content` field can be a `ReadableStream<Uint8Array>`, which is conveniently what's returned by [`fs.createReadStream`](https://nodejs.org/api/fs.html#fscreatereadstreampath-options)
-  const content = fs.createReadStream(filename)
+  const content = fs.createReadStream(filename);
 
   // Since our `filename` may contain the full path to a file, we use [`path.basename`](https://nodejs.org/api/path.html#pathbasenamepath-ext)
   // to get just the filename part of the path to use when packing the CAR.
-  const fileBasename = path.basename(filename)
+  const fileBasename = path.basename(filename);
 
   // The `file` object below now has the fields that ipfs-car expects for its `input` argument.
-  const file = { content, path: fileBasename }
+  const file = { content, path: fileBasename };
 
   const { root, car } = await packToBlob({
     // we need to pass an iterable (in this case, an array), even when packing a single file
-    input: [file] 
-  })
+    input: [file],
+  });
 
   // packToBlob returns a [`Blob`](https://developer.mozilla.org/en-US/docs/Web/API/Blob), but we want
   // a `Uint8Array` to pass to the client's `upload` method.
-  const buf = await car.arrayBuffer()
-  const carBytes = new Uint8Array(buf)
+  const buf = await car.arrayBuffer();
+  const carBytes = new Uint8Array(buf);
 
   // root is a CID object, but we only need the string form for this example
-  const rootCID = root.toString()
-  return { rootCID, carBytes }
+  const rootCID = root.toString();
+  return { rootCID, carBytes };
 }
 
-
 // call the entry-point function
-main()
+main();
+
